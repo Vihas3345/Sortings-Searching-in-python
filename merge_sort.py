d7a08be2cd16524297c4a82e96ba14a508b139f5
Vihas3345 <55677411+Vihas3345@users.noreply.github.com>
2020-06-11 10:15:28 +0530

    Add files via upload

---
+++ merge_sort.py
@@ -0,0 +1,31 @@
+def mergeSort(arr):
+    if len(arr)>1:
+        mid = len(arr)//2
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                arr[k]=lefthalf[i]
+                i=i+1
+            else:
+                arr[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            arr[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            arr[k]=righthalf[j]
+            j=j+1
+            k=k+1
+
+arr = [14,46,43,27,57,41,45,21,70]
+mergeSort(arr)
+print(arr)
